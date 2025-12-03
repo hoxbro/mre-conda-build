41bcae369fea87dc52c8f4e23e76c9603d4e9704
jaimergp <jaimergp@users.noreply.github.com>
2025-12-03 10:39:33 +0100

    Disable patch

--- scripts/conda/build.sh
+++ scripts/conda/build.sh
@@ -2,6 +2,6 @@
 
 set -euxo pipefail
 
-python -m pip install https://github.com/conda/conda-build/archive/refs/pull/5857/merge.tar.gz --no-deps
+# python -m pip install https://github.com/conda/conda-build/archive/refs/pull/5857/merge.tar.gz --no-deps
 python -m build --sdist .
 python -m conda build scripts/conda/recipe --no-anaconda-upload --no-verify -c conda-forge --package-format 2
